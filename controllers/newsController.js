39582479eefc4870a0015179e39d87c89e3650e7
Akshar Dave <akshar314@gmail.com>
2018-03-17 01:05:22 +0530

    return slug & title

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -4,26 +4,24 @@ var fs = require('fs');
 var request = require('request');
 var axios = require('axios');
 
-var scrapeUrl = "cnn url";
-
-
-function getArticles(html) {
-  var $ = cheerio.load(html);
-  // TODO:
-}
+var scrapeUrl = "https://edition.cnn.com/specials/politics/trumpmerica/";
+var scrapeUrl2 = "https://edition.cnn.com/specials/politics/president-donald-trump-45";
 
 newsController.handleGet = function(req, res, next) {
-  axios.get(scrapeUrl)
-    .then((response) => {
-      if(response.status === 200) {
-        var html = response.data;
-	getArticles(html)
+  var stories = [];
+  request(scrapeUrl, function(error, response, html) {
+    var $ = cheerio.load(html);
+    var headings = $('.cd__headline');
+    headings.each(function(i, element){
+      var slug =  $(this).children('a').attr('href')
+      var title = $(this).children('a').text();
+      stories.push({"headline": title, "slug": slug })
+    })
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ stories: stories }));
 
-      }
-    }, (error) => console.log(err) );
+  })
 
-  // res.setHeader('Content-Type', 'application/json');
-  // res.send(JSON.stringify({ foo: "bar" }));
 }
 
 module.exports = newsController;
