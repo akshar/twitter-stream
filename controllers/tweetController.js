8360b7aeb7e2f65f2d4ede028706c92015287c9a
Akshar Dave <akshar314@gmail.com>
2018-03-16 13:02:22 +0530

    pass screenName & limit params

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -8,12 +8,13 @@ var T = new Twit(config);
 function getTimeline(screenName, limit) {
   var options = { screen_name: screenName,
                   count: limit };
-
-  return T.get('statuses/user_timeline', options)
+  return T.get('statuses/user_timeline', options);
 }
 
 tweetController.handleGet =  function(req,res, next)  {
-  getTimeline("foo", 2).then(function(response) {
+  var screenName = req.params['screen_name'];
+  var limit = req.params.limit;
+  getTimeline(screenName, limit).then(function(response) {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ tweets: response }));
   });
