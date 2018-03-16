92eef13dca12f1ad4d3534602ad8c058737d3a9c
Akshar Dave <akshar314@gmail.com>
2018-03-16 12:41:03 +0530

    * /GET tweets

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,9 +1,22 @@
 var tweetController = {} ;
+var config = require("../config");
+var Twit = require('twit');
 
+var T = new Twit(config);
+
+
+function getTimeline(screenName, limit) {
+  var options = { screen_name: screenName,
+                  count: limit };
+
+  return T.get('statuses/user_timeline', options)
+}
 
 tweetController.handleGet =  function(req,res, next)  {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ a: 1 }));
+  getTimeline("foo", 2).then(function(response) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ tweets: response }));
+  });
 }
 
 module.exports = tweetController;
